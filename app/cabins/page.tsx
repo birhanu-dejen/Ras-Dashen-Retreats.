1b121af2b366e6bb9afa328d04b6141d0ab311de
birhanu-dejen <birhanudejen617@gmail.com>
2025-02-16 21:30:36 +0300

    adding interactivity to cabins

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,17 @@
-export default function Cabins() {
+import Counter from "../components/counter";
+export default async function Cabins() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
     </div>
   );
 }
