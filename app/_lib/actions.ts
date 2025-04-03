5fda049dfcdf1a247f53789faa94989bd4e3db4a
Birhanu Dejen <birhanudejen617@gmail.com>
2025-04-03 20:35:42 +0300

    modify updatebooking  server action

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -74,22 +74,30 @@ export async function deleteBooking(bookingId) {
   revalidatePath("/account/reservations");
 }
 
-export async function updateBooking(formData) {
-  const bookingId = Number(formData.get("bookingId"));
+export async function updateBooking(formData: FormData) {
+  const id = formData.get("bookingId"); 
+  const numGuests = formData.get("numGuests");
+  const observations = formData.get("observations");
 
-  const session = await auth();
-  if (!session) throw new Error("You must be logged in");
-
-  const guestBookings = await getBookings(session.user.guestId);
-  const guestBookingIds = guestBookings.map((booking) => booking.id);
-
-  if (!guestBookingIds.includes(bookingId))
-    throw new Error("You are not allowed to update this booking");
-
-  const updateData = {
-    numGuests: Number(formData.get("numGuests")),
-    observations: formData.get("observations").slice(0, 1000),
+  const updatedFields = {
+    numGuests: Number(numGuests),
+    observations,
   };
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(updatedFields)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+
+  return data;
+}
 
   const { error } = await supabase
     .from("bookings")
